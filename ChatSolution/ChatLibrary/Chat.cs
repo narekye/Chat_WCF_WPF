52f93647d85bfdeaeded6aa94083d0c443be05a0
narekye <yegoryan.narek@gmail.com>
2017-04-29 00:10:04 -0600

    :ok: added new methods :ok:

---
+++ ChatSolution/ChatLibrary/Chat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatLibrary
+{
+    public class Chat : IChat
+    {
+        private List<User> users;
+        public string GetData(string name, string message)
+        {
+            return "Sended from: " + name + " - " + message + " At: " + DateTime.Now.ToShortTimeString();
+        }
+
+        public bool Register(User user)
+        {
+            if (users.Exists(x => x.Name == user.Name))
+            {
+                return false;
+            }
+            users.Add(user);
+            return true;
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ChatSolution/ChatLibrary/IChat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatLibrary
+{
+    [ServiceContract]
+    public interface IChat
+    {
+        [OperationContract]
+        string GetData(string name, string message);
+    }
+}
